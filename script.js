371073d931e2baff10213e4d5556d7e859a4d934
AnarBattsengel0420 <146158513+AnarBattsengel0420@users.noreply.github.com>
2024-04-19 21:55:06 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+// JavaScript code to rotate images in the slider automatically
+const imageSlider = document.getElementById('imageSlider');
+let currentImageIndex = 0;
+const images = imageSlider.getElementsByTagName('img');
+
+function rotateImages() {
+  images[currentImageIndex].style.display = 'none';
+  currentImageIndex = (currentImageIndex + 1) % images.length;
+  images[currentImageIndex].style.display = 'block';
+}
+
+// Rotate images every 3 seconds
+setInterval(rotateImages, 3000);
 
